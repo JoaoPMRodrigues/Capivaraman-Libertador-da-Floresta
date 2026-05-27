211080731adb14420a30f769adcccad961028352
João Paulo (Pop-os) <jp.estudos17@gmail.com>
2026-05-27 20:36:07 -0300

    Direção do player e dos tiros adcionada

--- lib/bullet.py
+++ lib/bullet.py
@@ -3,7 +3,7 @@ from lib.entity import *
 
 class Bullet(Entity):
 
-    def __init__(self, window, x, y):
+    def __init__(self, window, x, y, direction):
 
         super().__init__(
             "sprites/bullet/bullet.png",
@@ -11,9 +11,11 @@ class Bullet(Entity):
             x,
             y
         )
-
+        self.direction = direction
         self.speed = 1000
 
     def update(self, dt):
-
-        self.sprite.x += self.speed * dt
+        if self.direction == "right":
+            self.sprite.x += self.speed * dt
+        else:
+            self.sprite.x -= self.speed * dt
--- lib/player.py
+++ lib/player.py
@@ -2,7 +2,7 @@ from lib.entity import *
 from lib.bullet import *
 from pplay.keyboard import *
 from pplay.sprite import *
-import os
+from os import listdir
 
 
 class Player(Entity):
@@ -10,28 +10,30 @@ class Player(Entity):
     def __init__(self, window):
 
         super().__init__(
-            "sprites/player/idle/idle_0.png",
+            "sprites/player/idle_right/idle_1.png",
             window,
             700,
             500
         )
 
         self.speed = 500
-
-        self.idle_frames = [
-            Sprite(f"sprites/player/idle/{img}")
-            for img in sorted(os.listdir("sprites/player/idle"))
+        self.direction = "right"
+        self.idle_right_frames = [
+            Sprite(f"sprites/player/idle_right/{img}")
+            for img in sorted(listdir("sprites/player/idle_right"))
+        ]
+        self.idle_left_frames = [
+            Sprite(f"sprites/player/idle_left/{img}")
+            for img in sorted(listdir("sprites/player/idle_left"))
         ]
 
         self.walk_right_frames = [
             Sprite(f"sprites/player/walk_right/{img}")
-            for img in sorted(os.listdir("sprites/player/walk_right"))
+            for img in sorted(listdir("sprites/player/walk_right"))
         ]
 
         self.walk_left_frames = [Sprite(f"sprites/player/walk_left/{img}")
-                                 for img in sorted(os.listdir("sprites/player/walk_left"))]
-
-        self.current_animation = self.idle_frames
+                                 for img in sorted(listdir("sprites/player/walk_left"))]
 
         self.frame = 0
         self.animation_timer = 0
@@ -65,37 +67,67 @@ class Player(Entity):
 
         moving_right = moving_left = False
 
+
+# Movimento horizontal
+
         if keyboard.key_pressed("A"):
+
             self.sprite.x -= self.speed * dt
             moving_left = True
+            self.direction = "left"
+
+            # Barreira esquerda
+            if self.sprite.x < 0:
+                self.sprite.x = 0
 
         if keyboard.key_pressed("D"):
+
             self.sprite.x += self.speed * dt
             moving_right = True
+            self.direction = "right"
+
+            # Barreira direita
+            if self.sprite.x + self.sprite.width > window.width:
+                self.sprite.x = window.width - self.sprite.width
+
+        # Movimento vertical
 
         if keyboard.key_pressed("W"):
+
             self.sprite.y -= self.speed * dt
 
+            # Barreira superior
+            if self.sprite.y < 0:
+                self.sprite.y = 0
+
         if keyboard.key_pressed("S"):
+
             self.sprite.y += self.speed * dt
 
-        # Troca animação
+            # Barreira inferior
+            if self.sprite.y + self.sprite.height > window.height:
+                self.sprite.y = window.height - self.sprite.height
 
-        if moving_right:
-            self.current_animation = self.walk_right_frames
-        elif moving_left:
-            self.current_animation = self.walk_left_frames
+        # Troca animação
+        if self.direction == "right":
+            if moving_right:
+                self.current_animation = self.walk_right_frames
+            else:
+                self.current_animation = self.idle_right_frames
         else:
-            self.current_animation = self.idle_frames
+            if moving_left:
+                self.current_animation = self.walk_left_frames
+            else:
+                self.current_animation = self.idle_left_frames
 
         self.animate(dt)
 
         if keyboard.key_pressed("SPACE") and self.shoot_cooldown < 0:
-
             bullet = Bullet(
                 window,
-                self.sprite.x + self.sprite.width,
-                self.sprite.y + self.sprite.height / 2
+                self.sprite.x,
+                self.sprite.y + self.sprite.height / 2,
+                self.direction
             )
 
             self.bullets.append(bullet)
@@ -104,4 +136,8 @@ class Player(Entity):
         for bullet in self.bullets:
             bullet.update(dt)
             bullet.draw()
+
+            if bullet.sprite.x < 0 or bullet.sprite.x > window.width+bullet.sprite.x:
+                self.bullets.remove(bullet)
+
         self.shoot_cooldown -= dt
