687dbe329e1bea3b959d5c27729e610a2fc70c41
João Paulo (Pop-os) <jp.estudos17@gmail.com>
2026-05-27 17:14:10 -0300

    Cria animação pro player

--- lib/player.py
+++ lib/player.py
@@ -1,7 +1,7 @@
-# lib/player.py
-
-from pplay.keyboard import *
 from lib.entity import *
+from pplay.keyboard import *
+from pplay.sprite import *
+import os
 
 
 class Player(Entity):
@@ -9,7 +9,7 @@ class Player(Entity):
     def __init__(self, window):
 
         super().__init__(
-            "sprites/player/idle/idle1.png",
+            "sprites/player/idle/idle_0.png",
             window,
             700,
             500
@@ -17,30 +17,73 @@ class Player(Entity):
 
         self.speed = 500
 
+        self.idle_frames = [
+            Sprite(f"sprites/player/idle/{img}")
+            for img in sorted(os.listdir("sprites/player/idle"))
+        ]
+
+        self.walk_right_frames = [
+            Sprite(f"sprites/player/walk_right/{img}")
+            for img in sorted(os.listdir("sprites/player/walk_right"))
+        ]
+
+        self.walk_left_frames = [Sprite(f"sprites/player/malk_left")
+                                 for img in sorted(os.listdir("sprites/player/malk_left"))]
+
+        self.current_animation = self.idle_frames
+
+        self.frame = 0
+        self.animation_timer = 0
+        self.animation_speed = 0.12
+
+    def animate(self, dt):
+
+        self.animation_timer += dt
+
+        if self.animation_timer >= self.animation_speed:
+
+            self.animation_timer = 0
+
+            self.frame += 1
+
+            if self.frame >= len(self.current_animation):
+                self.frame = 0
+
+            old_x = self.sprite.x
+            old_y = self.sprite.y
+
+            self.sprite = self.current_animation[self.frame]
+
+            self.sprite.x = old_x
+            self.sprite.y = old_y
+
     def update(self, window, keyboard, dt):
 
-        if keyboard.key_pressed("A") or keyboard.key_pressed("LEFT"):
+        moving_right = moving_left = False
+
+        if keyboard.key_pressed("A"):
             self.sprite.x -= self.speed * dt
+            moving_left = True
 
-        if keyboard.key_pressed("D") or keyboard.key_pressed("RIGHT"):
+        if keyboard.key_pressed("D"):
             self.sprite.x += self.speed * dt
+            moving_right = True
 
-        if keyboard.key_pressed("W") or keyboard.key_pressed("UP"):
+        if keyboard.key_pressed("W"):
             self.sprite.y -= self.speed * dt
+            moving = True
 
-        if keyboard.key_pressed("S") or keyboard.key_pressed("DOWN"):
+        if keyboard.key_pressed("S"):
             self.sprite.y += self.speed * dt
+            moving = True
 
-        # Limites da tela
+        # Troca animação
 
-        if self.sprite.x < 0:
-            self.sprite.x = 0
+        if moving_right:
+            self.current_animation = self.walk_right_frames
+        elif moving_left:
+            self.current_animation = self.walk_left_frames
+        else:
+            self.current_animation = self.idle_frames
 
-        if self.sprite.y < 0:
-            self.sprite.y = 0
-
-        if self.sprite.x + self.sprite.width > window.width:
-            self.sprite.x = window.width - self.sprite.width
-
-        if self.sprite.y + self.sprite.height > window.height:
-            self.sprite.y = window.height - self.sprite.height
+        self.animate(dt)
